7188bcf0734ae3c86c92a63d33f12b965fc26fc9
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-05-06 22:04:07 -0300

    updates

--- javascript/script.js
+++ javascript/script.js
@@ -23,3 +23,18 @@ window.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('resize', resizeIframe);
   });
   
+
+  var navItems = document.querySelectorAll('.nav-item');
+
+    // Adiciona um ouvinte de evento de clique a cada item de navegação
+    navItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            // Remove a classe 'active' de todos os itens de navegação
+            navItems.forEach(function(navItem) {
+                navItem.classList.remove('active');
+            });
+
+            // Adiciona a classe 'active' ao item clicado
+            this.classList.add('active');
+        });
+    });
